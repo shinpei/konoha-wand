560303c8622f959f94e4099b3c08f3b75a4126da
tsunade <tsunade@localhost.localdomain>
2009-04-12 00:25:36 +0900

    selfclean.py

---
+++ imagemagick/selfclean.py
@@ -0,0 +1,15 @@
+import os, sys
+
+input = open(sys.argv[1], 'r')
+output = open("out", 'w')
+L = input.readlines()
+input.close()
+for s in xrange(len(L)):
+	if 'self' in L[s]:
+		line = L[s].replace('self,','')
+		if 'self' in line:
+			line = line.replace('self','')
+		output.writelines(line)
+	else:
+		output.writelines(L[s])
+output.close()
