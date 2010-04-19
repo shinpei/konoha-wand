59dc956df3448912c56aa1ba92113585cd739053
shinpei <shinpei@ynu.ac.jp>
2010-04-19 21:51:34 +0900

    simple chat

---
+++ ape/SimpleChat/simplechat.js
@@ -0,0 +1,16 @@
+/**
+ * @author shinpei
+ */
+
+ APE.SimpleChat = new Class({
+ 	Extends: APE.Client,
+ 	Implements: Options,
+ 	
+ 	options:{
+ 		container: null,
+ 		logs_limit: 10,
+ 		container: document.body
+ 	}
+ 	
+ 	
+ });
