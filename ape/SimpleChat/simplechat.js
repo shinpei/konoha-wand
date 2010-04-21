f6439cea1eeef58e2ee50c0d17dc8284317d2f56
shinpei <shinpei@ynu.ac.jp>
2010-04-22 00:25:18 +0900

    sc

--- ape/SimpleChat/simplechat.js
+++ ape/SimpleChat/simplechat.js
@@ -10,7 +10,43 @@
  		container: null,
  		logs_limit: 10,
  		container: document.body
+ 	},
+ 	
+ 	initialize: function(options){
+ 		this.setOptions(options);
+ 		
+ 		this.els = {};
+ 		this.currentPipe = null;
+ 		this.logging = true;
+ 		
+ 		this.addEvent('ready', this.createChat);
+ 		//this.onRaw('data', this.rawData);
+ 		//this.onCmd('send', this.cmdSend); 		
+ 	},
+ 	
+ 	start: function() {
+ 		var opt = {'sendStack':false, 'request': 'stack'};
+ 		this.core.start({'name':this.options.name}, opt);
+ 		
+ 		this.core.request.stack.send();
+ 	},
+ 	
+ 	rawData: function(raw, pipe) {
+ 		this.writeMessage(pipe, raw.data.msg);
+ 	},
+ 	
+ 	writeMessage: function(pipe, message) {
+ 		var msg = new Element('div', {'class':'sc_message_container'});
+ 		var cnt = new Element('div', {'class':'msg_top'}).inject(msg);
+ 		new Element('div', {
+ 			'text':message,
+ 			'class':'ape_message'
+ 		}).inject(cnt);
+ 		
+ 	},
+ 	
+ 	createChat: function(){
+ 		this.els.sendboxContainer = new Element('div', {'id':'sc_sendbox_container'}).inject(this.options.container);
+ 		this.els.sendBox = new Element('div', {'id':'sc_sendbox'}).inject(this.els.sendboxContainer, 'bottom');
  	}
- 	
- 	
  });
